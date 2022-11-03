f1a866d35414948abd3635a3eef237f7e4a61abf
Joaquin Palacios <68148163+JoaquinPalacios@users.noreply.github.com>
2022-11-04 06:59:59 +1000

    Diego's changes on reachout component commited

--- components/ReachOut/index.tsx
+++ components/ReachOut/index.tsx
@@ -2,8 +2,8 @@ const ReachOut = () => {
     return (
         <div className='reachOut'>
             <div className='reachOut-container'>
-                  <h3>Queremos saber de vos</h3>
-                <button className='button'>Enviar mensaje</button>
+                <h3>Queremos saber de vos</h3>
+                <button className='button'>Enviar mensaje</button>
             </div>
         </div>
     );
