af4ac9a29800ac4c431559f673043410b9e92089
Joaquin Palacios <68148163+JoaquinPalacios@users.noreply.github.com>
2022-11-04 07:19:32 +1000

    Footer started

--- components/Footer/index.tsx
+++ components/Footer/index.tsx
@@ -1,7 +1,24 @@
+import Image from 'next/image';
+
 export interface FooterProps {}
 
 const Footer: React.FC<FooterProps> = () => {
-    return <h1>Footer</h1>;
+    return (
+        <footer className='footer'>
+            <div className='footer-container'>
+                <div className='footer-logo'>
+                    <Image src='/images/footer-logo.svg' alt='Logo' width='233' height='111' />
+                </div>
+                <div className='footer-links'>
+                    <a href='#'>Direccion</a>
+                    <a href='#'>Email</a>
+                    <a href='#'>Telefono</a>
+                </div>
+                <button className='btn-login'>Login clientes</button>
+                <div className='footer-social'></div>
+            </div>
+        </footer>
+    );
 };
 
 export default Footer;
