615603b1152b6d706cb1876d46cdc21d84b36158
DiegoReyesCipo <Tu email aca>
2022-11-05 16:18:52 -0300

    ReachOut component image and Css

--- components/ReachOut/index.tsx
+++ components/ReachOut/index.tsx
@@ -3,7 +3,7 @@ const ReachOut = () => {
         <div className='reachOut'>
             <div className='reachOut-container'>
                 <h3>Queremos saber de vos</h3>
-                <button className='button'>Enviar mensaje</button>
+                <button className='button'>Enviar mensaje</button>
             </div>
         </div>
     );
